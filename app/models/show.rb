6979b201558e1312e6cce08727a40d5911e63c9d
Tim Quimbo <tquimbo1@gmail.com>
2021-04-21 22:49:50 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,15 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+
+    def actors_list
+    end
+
+    def genre
+
+    end
+
+    def build_network
+    end
+    
 end
